e294f2817e45d2b9f8493e22daa9c5f58d6afb5f
JoshBirkholtz <josh.birkholtz@gmail.com>
2025-02-18 20:32:47 +0200

    Updated cors options for new render url

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 const corsOptions = {
     origin: process.env.NODE_ENV === 'production' 
-        ? ['https://your-client-app.onrender.com'] 
+        ? ['https://cape-town-festivals.onrender.com'] 
         : "http://localhost:5173",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
